cb52e163e3a39c333719068367583cdc10b3c56b
Rijul Gupta <rijulg@gmail.com>
2017-06-07 10:21:52 +1000

    Added GNU GPL

--- ScrewDriver.ino
+++ ScrewDriver.ino
@@ -1,3 +1,26 @@
+/**
+        * ScrewDriver
+        * This file is a part of ScrewD (Arduino Implementation)
+        *
+        * For project description read (https://bitbucket.org/rijulg/smart-screwdriver/overview)
+        *
+        * @author Rijul Gupta (rijulg@gmail.com)
+        * @since June 2017
+        *
+        * ScrewD is free software: you can redistribute it and/or modify
+        * it under the terms of the GNU General Public License as published by
+        * the Free Software Foundation, either version 3 of the License, or
+        * (at your option) any later version.
+        *
+        * ScrewD is distributed in the hope that it will be useful,
+        * but WITHOUT ANY WARRANTY; without even the implied warranty of
+        * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        * GNU General Public License for more details.
+        *
+        * For the GNU General Public License associated with ScrewD,  
+        * visit <http://www.gnu.org/licenses/>.
+*/
+
 #define OneTurn 42
 #define OnePitch 7
 #define ScrewLength 4
